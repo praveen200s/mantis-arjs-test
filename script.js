837feda23669d23a59e416c6f11af3518acd247d
praveen200s <praveen200sinha@gmail.com>
2020-09-09 23:07:04 +0530

    0.009

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-document.querySelector('#apartment').addEventListener('click', moveRight());
-
 let flag = true;
 
-function moveRight() {
-    debugger;
+let apartment = document.querySelector('#apartment');
+
+apartment.addEventListener('click', function () {
+
     if (flag) {
         flag = !flag;
-        this.setAttribute('position', '5 0 0');;
+        //this.setAttribute('position', '5 0 0');
+        this.setAttribute('animation__pos', {
+            property: 'position',
+            dur: 2000,
+            easing: 'easeInOutSine',
+            to: '5 0 0'
+        });
     } else {
         flag = !flag;
-        this.setAttribute('position', '-5 0 0');
+        //this.setAttribute('position', '-5 0 0');
+        this.setAttribute('animation__pos', {
+            property: 'position',
+            dur: 2000,
+            easing: 'easeInOutSine',
+            to: '-5 0 0'
+        });
     }
-}
+});
