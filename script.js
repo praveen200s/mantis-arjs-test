df05ab043db3296fc4b1da8c1490fc8d70fc8d09
praveen200s <praveen200sinha@gmail.com>
2020-09-13 23:42:10 +0530

    0.010

--- script.js
+++ script.js
@@ -1,26 +1,177 @@
-let flag = true;
+var scene = document.querySelector('a-scene');
 
-let apartment = document.querySelector('#apartment');
+if (scene.hasLoaded) {
+    run();
+} else {
+    scene.addEventListener('loaded', run);
+}
 
-apartment.addEventListener('click', function () {
+function run() {
 
-    if (flag) {
-        flag = !flag;
-        //this.setAttribute('position', '5 0 0');
-        this.setAttribute('animation__pos', {
-            property: 'position',
+    let apartment = document.querySelector("#apartment");
+    let flat = document.querySelector("#flat");
+    let card = document.querySelector("#card");
+    let but_1 = document.querySelector("#but_1");
+    let but_2 = document.querySelector("#but_2");
+    let but_3 = document.querySelector("#but_3");
+    let aflag = true;
+    let fflag = true;
+
+    apartment.addEventListener("click", function (event) {
+        //open card
+        //close card
+        console.log("A1");
+        if (aflag) {
+            card.setAttribute("animation__visible", {
+                property: "scale",
+                dur: 2000,
+                easing: "easeInOutSine",
+                to: "4 2 0.2"
+            });
+            card.setAttribute("animation__position", {
+                property: "position",
+                dur: 2000,
+                easing: "easeInOutSine",
+                to: '0 1 3'
+            });
+        } else {
+            card.setAttribute("animation__visible", {
+                property: "scale",
+                dur: 2000,
+                easing: "easeInOutSine",
+                to: "0.01 0.01 0.01"
+            });
+            card.setAttribute("animation__position", {
+                property: "position",
+                dur: 2000,
+                easing: "easeInOutSine",
+                to: '0 1 -3'
+            });
+        }
+        aflag = !aflag;
+    });
+    flat.addEventListener("click", function (event) {
+        //open card
+        //close card
+        console.log("A2");
+        if (fflag) {
+            card.setAttribute("animation__visible", {
+                property: "scale",
+                dur: 2000,
+                easing: "easeInOutSine",
+                to: "4 2 0.2"
+            });
+            card.setAttribute("animation__position", {
+                property: "position",
+                dur: 2000,
+                easing: "easeInOutSine",
+                to: '0 1 3'
+            });
+        } else {
+            card.setAttribute("animation__visible", {
+                property: "scale",
+                dur: 2000,
+                easing: "easeInOutSine",
+                to: "0.01 0.01 0.01"
+            });
+            card.setAttribute("animation__position", {
+                property: "position",
+                dur: 2000,
+                easing: "easeInOutSine",
+                to: '0 1 -3'
+            });
+        }
+        fflag = !fflag;
+    });
+
+    but_1.addEventListener("click", function (event) {
+        //open apartment
+        //close card
+        console.log("B1");
+        card.setAttribute("animation__visible", {
+            property: "scale",
             dur: 2000,
-            easing: 'easeInOutSine',
-            to: '5 0 0'
+            easing: "easeInOutSine",
+            to: "0.01 0.01 0.01"
         });
-    } else {
-        flag = !flag;
-        //this.setAttribute('position', '-5 0 0');
-        this.setAttribute('animation__pos', {
-            property: 'position',
+        card.setAttribute("animation__position", {
+            property: "position",
             dur: 2000,
-            easing: 'easeInOutSine',
-            to: '-5 0 0'
+            easing: "easeInOutSine",
+            to: '0 1 -3'
         });
-    }
-});
+        apartment.setAttribute("animation__visible", {
+            property: "scale",
+            dur: 2000,
+            easing: "easeInOutSine",
+            to: "0.2 0.2 0.2"
+        });
+        apartment.setAttribute("animation__position", {
+            property: "position",
+            dur: 2000,
+            easing: "easeInOutSine",
+            to: '0 0 0'
+        });
+        flat.setAttribute("animation__visible", {
+            property: "scale",
+            dur: 2000,
+            easing: "easeInOutSine",
+            to: "0.01 0.01 0.01"
+        });
+        flat.setAttribute("animation__position", {
+            property: "position",
+            dur: 2000,
+            easing: "easeInOutSine",
+            to: '0 0 -5'
+        });
+    });
+    but_2.addEventListener("click", function (event) {
+        //open flat
+        //close card
+        console.log("B2");
+        card.setAttribute("animation__visible", {
+            property: "scale",
+            dur: 2000,
+            easing: "easeInOutSine",
+            to: "0.01 0.01 0.01"
+        });
+        card.setAttribute("animation__position", {
+            property: "position",
+            dur: 2000,
+            easing: "easeInOutSine",
+            to: '0 1 -3'
+        });
+        flat.setAttribute("animation__visible", {
+            property: "scale",
+            dur: 2000,
+            easing: "easeInOutSine",
+            to: "0.2 0.2 0.2"
+        });
+        flat.setAttribute("animation__position", {
+            property: "position",
+            dur: 2000,
+            easing: "easeInOutSine",
+            to: '0 0 0'
+        });
+        apartment.setAttribute("animation__visible", {
+            property: "scale",
+            dur: 2000,
+            easing: "easeInOutSine",
+            to: "0.01 0.01 0.01"
+        });
+        apartment.setAttribute("animation__position", {
+            property: "position",
+            dur: 2000,
+            easing: "easeInOutSine",
+            to: '0 0 -5'
+        });
+    });
+    but_3.addEventListener("click", function (event) {
+        //open link
+        //close card
+        console.log("B3");
+        window.open(
+            "https://www.google.com", "_blank");
+    });
+
+}
